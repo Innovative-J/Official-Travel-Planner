f51a73ba6482a1950c67f93835dc3c0602b0f097
Innovative-J <juan.cia05678@gmail.com>
2024-06-13 00:15:54 -0500

    updated login page and dashboard page

--- server.js
+++ server.js
@@ -8,20 +8,13 @@ const app = express();
 const port = 3000;
 const hbs = exphbs.create({});
 
-app.engine(
-  "handlebars",
-  exphbs({
-    // extname: "handlebars",
-    // defaultLayout: "",
-    // // layoutsDir: "views/layouts",
-  })
-);
+app.engine("handlebars", hbs.engine);
 
 app.set("view engine", "handlebars");
 app.use(express.static(path.join(__dirname, "public")));
 console.log("directory", path.join(__dirname, "public"));
 // app.set("views", path.join(__dirname, "/views"));
-app.set("views", "./views");
+// app.set("views", "./views");
 
 app.use(routes);
 
