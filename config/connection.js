d972263f837ae09a84d6cce947a3ee68cef2dab8
Devinthedevleoper <dxtofocused@gmail.com>
2024-07-18 18:55:42 -0500

    Devin Haynes's add comments to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize'); // Import the Sequelize library
 require('dotenv').config(); // Load environment variables from a .env file
 
+// Create a new Sequelize instance based on whether a database URL is provided in the environment variables
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(
