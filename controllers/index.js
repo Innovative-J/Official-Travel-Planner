9768a0d74bbe318e06d0493d53832995933f5671
Devinthedevleoper <dxtofocused@gmail.com>
2024-07-18 18:52:05 -0500

    Devin Haynes's add comments to the index.js

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,4 @@ const homeRoutes = require("./home-routes.js"); // Import the home-routes module
 
 router.use("/", homeRoutes); // Use the imported home-routes for the root path
 
-module.exports = router;
+module.exports = router; // Export the router to be used in other parts of the application
