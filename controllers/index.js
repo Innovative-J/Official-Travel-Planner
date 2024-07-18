44b305f2f3e1eec5974f865c00001bc4f8edee1e
Devinthedevleoper <dxtofocused@gmail.com>
2024-07-18 18:48:58 -0500

    Devin Haynes's adding comments to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router(); // Import the Express Router
 
-const homeRoutes = require("./home-routes.js");
+const homeRoutes = require("./home-routes.js"); // Import the home-routes module
 
 router.use("/", homeRoutes);
 
