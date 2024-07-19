f2529a2acfb3a9dd5d5e4029d0910ef3700534c9
Devinthedevleoper <dxtofocused@gmail.com>
2024-07-18 19:13:25 -0500

    Devin Haynes's adding comments to index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,13 @@
-const router = require('express').Router();
+const router = require('express').Router(); // Import the Express Router
 
-const userRoutes = require('./user-routes');
+const userRoutes = require('./user-routes'); // Import the user-routes module
 
-router.use('/users', userRoutes);
+router.use('/users', userRoutes); // Use the imported user-routes for the /users path
 
-module.exports = router;
+module.exports = router; // Export the router to be used in other parts of the application
+
+
+
+
+
+
